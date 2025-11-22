2e0897086fb9497b7d2f80f9825e1ba35fd930d1
BuildTools <unconfigured@null.spigotmc.org>
2025-11-22 17:18:49 -0500

    Update with Supabase realtime +

--- app/api/requests/route.js
+++ app/api/requests/route.js
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
