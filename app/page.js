6a2a2cf7696d7e9abc0cc537fb4e26443fa4f247
James Abbott <abbottjames319@gmail.com>
2025-11-23 00:50:11 -0500

    Better UI6

--- app/page.js
+++ app/page.js
@@ -309,7 +309,7 @@ export default function Dashboard() {
 
                   {/* Thumbnail */}
                   <div className="flex-shrink-0">
-                    <div className="h-full aspect-square max-h-[140px] rounded-xl overflow-hidden border-2 border-[#2a2a40] group-hover:border-[#ff4da3] transition-all shadow-lg">
+                    <div className="h-full aspect-square max-h-[200px] rounded-xl overflow-hidden border-2 border-[#2a2a40] group-hover:border-[#ff4da3] transition-all shadow-lg">
                       {req.thumbnail ? (
                         <img
                           src={req.thumbnail}
