a24d3930a8c34536e609e679ccbedc613e045489
James Abbott <abbottjames319@gmail.com>
2026-01-07 18:25:18 -0500

    Chat height8

--- components/dashboard/StatsSidebar.js
+++ components/dashboard/StatsSidebar.js
@@ -88,7 +88,7 @@ export default function StatsSidebar({
       {djProfile?.id && (
         <LiveChat 
           djId={djProfile.id} 
-          className="h-[calc(100vh-455px)] min-h-[300px]" 
+          className="h-[calc(100vh-450px)] min-h-[300px]" 
         />
       )}
     </div>
